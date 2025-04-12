1382e95a4e2bfb26e939a917ec98d0b8ecf14aeb
Geoo Chi <geoochi@geoochi.com>
2025-04-12 10:46:54 +0800

    update

---
+++ src/endpoints/notFound.ts
@@ -0,0 +1,30 @@
+import { OpenAPIRoute } from 'chanfana'
+import { z } from 'zod'
+
+export class NotFound extends OpenAPIRoute {
+  schema = {
+    tags: ['404'],
+    summary: '404 Not Found',
+    responses: {
+      '404': {
+        description: 'Not Found',
+        content: {
+          'application/json': {
+            schema: z.object({
+              success: z.boolean(),
+              message: z.string(),
+            }),
+          },
+        },
+      },
+    },
+  }
+
+  async handle(c) {
+    // const data = await this.getValidatedData<typeof this.schema>()
+    return {
+      success: false,
+      message: 'Not Found',
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from 'hono'
 import { fromHono } from 'chanfana'
+
 import { Todos } from './endpoints/todos'
 import { Count } from './endpoints/count'
 import { CountPlus } from './endpoints/countPlus'
+import { NotFound } from './endpoints/notFound'
 
 const app = new Hono()
 const openapi = fromHono(app, {
@@ -12,5 +14,6 @@ const openapi = fromHono(app, {
 openapi.get('/v1/todos', Todos)
 openapi.get('/v1/count', Count)
 openapi.get('/v1/count-plus', CountPlus)
+openapi.get('/*', NotFound)
 
 export default app
